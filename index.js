73564d7c8bcba6fdc7302edf5491cad0389984d4
santmorales2025 <santorales2018@gmail.com>
2025-04-19 12:59:22 -0600

    second

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 // Iteration 1: Names and Input
-let hacker1 = ""
-console.log("the driver's name is XXXX")
-let hacker2 = ""
-console.log("the navigator's name is YYYY")
+const hacker1 = "Cesar"
+console.log("the driver's name is Cesar")
+const hacker2 = "Santiago"
+console.log("the navigator's name is Santiago")
 
 // Iteration 2: Conditionals
-
+if (hacker1 > hacker2) {console.log(´The driver has the longest name, it has ${hacker1.length} characters´)}
+else if (hacker2 > hacker1) {console.log(´It seems that the navigator has the longest name, it has ${hacker2.length} characters´)}
+else {console.log("Wow, you both have equally long names, ${hacker1.length} characters!")}
 
 // Iteration 3: Loops
+console.log(hacker1.toUpperCase)
+console.log(hacker2.toUpperCase)
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+  }
+  let myString = "cesar";
+  let reversedString = reverseString(myString);
+  console.log(reversedString);
+  let myString = "santiago";
+  let reversedString = reverseString(myString);
+  console.log(reversedString);
+
+if $(hacker1 > hacker2) {console.log("The driver's name goes first")}
+else if (hacker2 > hacker1) {console.log("Yo, the navigator goes first, definitely")}
+else {console.log("What?! You both have the same name?")}
