55e94db2db155080f8e1a12afa2f4c9a5140d5d7
santmorales2025 <santorales2018@gmail.com>
2025-04-19 12:57:50 -0600

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-
+let hacker1 = ""
+console.log("the driver's name is XXXX")
+let hacker2 = ""
+console.log("the navigator's name is YYYY")
 
 // Iteration 2: Conditionals
 
